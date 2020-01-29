8c7723ee7165fb26becd0e390542bbe4f0f895ee
fabian <fpicneo87@gmail.com>
2020-01-28 18:02:12 -0600

    se agrego la libreria de comunicacion para el puerto serial

--- src/main/java/com/fpiceno/abastos/test/comSerialTest.java
+++ src/main/java/com/fpiceno/abastos/test/comSerialTest.java
@@ -3,10 +3,14 @@
 //
 //import java.util.logging.Level;
 //import java.util.logging.Logger;
+//import javafx.application.Platform;
 //import javafx.collections.FXCollections;
 //import javafx.collections.ObservableList;
 //import javafx.scene.control.Label;
 //import jssc.SerialPort;
+//import static jssc.SerialPort.MASK_RXCHAR;
+//import jssc.SerialPortEvent;
+//import jssc.SerialPortEventListener;
 //import jssc.SerialPortException;
 //import jssc.SerialPortList;
 //
@@ -78,6 +82,70 @@
 //            }
 //            catch (SerialPortException ex) {
 //                System.out.println("Error in receiving string from COM-port: " + ex);
+//            }
+//        }
+//    }
+//    
+//    
+//    public boolean connectArduino(String port){
+//        
+//        System.out.println("connectArduino");
+//        
+//        boolean success = false;
+//        SerialPort serialPort = new SerialPort(port);
+//        try {
+//            serialPort.openPort();
+//            serialPort.setParams(
+//                    SerialPort.BAUDRATE_9600,
+//                    SerialPort.DATABITS_8,
+//                    SerialPort.STOPBITS_1,
+//                    SerialPort.PARITY_NONE);
+//            serialPort.setEventsMask(MASK_RXCHAR);
+//            serialPort.addEventListener((SerialPortEvent serialPortEvent) -> {
+//                if(serialPortEvent.isRXCHAR()){
+//                    try {
+//                        String st = serialPort.readString(serialPortEvent
+//                                .getEventValue());
+//                        System.out.println(st);
+//                        
+//                        //Update label in ui thread
+//                        Platform.runLater(() -> {
+//                            labelValue.setText(st);
+//                        });
+//                        
+//                    } catch (SerialPortException ex) {
+//                        Logger.getLogger(JavaFX_jssc_Uno.class.getName())
+//                                .log(Level.SEVERE, null, ex);
+//                    }
+//                    
+//                }
+//            });
+//            
+//            arduinoPort = serialPort;
+//            success = true;
+//        } catch (SerialPortException ex) {
+//            Logger.getLogger(JavaFX_jssc_Uno.class.getName())
+//                    .log(Level.SEVERE, null, ex);
+//            System.out.println("SerialPortException: " + ex.toString());
+//        }
+//
+//        return success;
+//    }
+//    
+//      public void disconnectArduino(){
+//        
+//        System.out.println("disconnectArduino()");
+//        if(arduinoPort != null){
+//            try {
+//                arduinoPort.removeEventListener();
+//                
+//                if(arduinoPort.isOpened()){
+//                    arduinoPort.closePort();
+//                }
+//                
+//            } catch (SerialPortException ex) {
+//                Logger.getLogger(JavaFX_jssc_Uno.class.getName())
+//                        .log(Level.SEVERE, null, ex);
 //            }
 //        }
 //    }
