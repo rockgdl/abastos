12c843544254d7e41c4133da2b31d79199f6bd84
fabian <fpicneo87@gmail.com>
2020-02-07 17:23:49 -0600

    logs y regreso a pantalla

--- src/main/java/com/fpiceno/abastos/controller/SerialFxController.java
+++ src/main/java/com/fpiceno/abastos/controller/SerialFxController.java
@@ -6,16 +6,23 @@ package com.fpiceno.abastos.controller;
  * and open the template in the editor.
  */
 import com.fazecast.jSerialComm.SerialPort;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 
 /**
  * FXML Controller class
@@ -30,6 +37,8 @@ public class SerialFxController implements Initializable {
             private final org.apache.log4j.Logger LOG= org.apache.log4j.Logger.getLogger(SerialFxController.class.getSimpleName());
 
     @FXML ComboBox portList;
+        @FXML
+    private AnchorPane anchorPane;
     
     private ObservableList <ArrayList> oblistBajas= FXCollections.observableArrayList();
     @Override
@@ -56,7 +65,16 @@ public class SerialFxController implements Initializable {
     
     @FXML private void setConfiguracion()
     {
-        LOG.info("REGRESANDO a la pantalla de configuracion con los parametros para la base de datos ");
+                try {
+                    LOG.info("REGRESANDO a la pantalla de configuracion con los parametros para la base de datos ");
+                    
+                    FlowPane pane;
+                    LOG.info("cargando vista de productos ");
+                    pane= FXMLLoader.load(getClass().getResource("/fxml/Productos.fxml"));
+                    anchorPane.getChildren().setAll(pane);
+                } catch (IOException ex) {
+                    Logger.getLogger(SerialFxController.class.getName()).log(Level.SEVERE, null, ex);
+                }
     }
     
 }
